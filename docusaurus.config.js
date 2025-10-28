0bfbb8adb2e6a3eeda6e745d4a2036c57311fbfc
whoisnian <zhuchangbao1998@gmail.com>
2025-10-28 23:25:45 +0800

    CHG: docusaurus.config for software/system/programming/others categories

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,123 +1,82 @@
-// @ts-check
-// `@type` JSDoc annotations allow editor autocompletion and type checking
-// (when paired with `@ts-check`).
-// There are various equivalent ways to declare your Docusaurus config.
-// See: https://docusaurus.io/docs/api/docusaurus-config
-
-import {themes as prismThemes} from 'prism-react-renderer';
+import { themes as prismThemes } from 'prism-react-renderer';
 
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Snippets',
+  tagline: 'Collection of knowledge snippets',
   favicon: 'img/favicon.ico',
 
-  // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
-  future: {
-    v4: true, // Improve compatibility with the upcoming Docusaurus v4
-  },
+  future: { v4: true }, // https://docusaurus.io/docs/api/docusaurus-config#future
 
-  // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
+  url: 'https://snippets.whoisnian.com',
   baseUrl: '/',
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'whoisnian', // Usually your GitHub org/user name.
+  projectName: 'snippets', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: 'zh-CN',
+    locales: ['zh-CN'],
   },
 
   presets: [
     [
       'classic',
       /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
+      {
         docs: {
           sidebarPath: './sidebars.js',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          editUrl: ({ versionDocsDirPath, docPath }) => {
+            return `https://github.com/whoisnian/snippets/edit/master/${versionDocsDirPath}/${docPath}`
+          },
         },
-        theme: {
-          customCss: './src/css/custom.css',
-        },
-      }),
+        theme: { customCss: './src/css/custom.css' },
+      },
     ],
   ],
 
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
-      // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
-      colorMode: {
-        respectPrefersColorScheme: true,
-      },
+      image: 'img/logo.svg',
+      colorMode: { respectPrefersColorScheme: true },
       navbar: {
-        title: 'My Site',
-        logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.svg',
-        },
+        title: 'Snippets',
+        logo: { alt: 'Website Logo', src: 'img/logo.svg' },
         items: [
-          {
-            type: 'docSidebar',
-            sidebarId: 'tutorialSidebar',
-            position: 'left',
-            label: 'Tutorial',
-          },
-          {
-            href: 'https://github.com/facebook/docusaurus',
-            label: 'GitHub',
-            position: 'right',
-          },
+          { label: '软件', to: '/docs/software', position: 'left' },
+          { label: '系统', to: '/docs/system', position: 'left' },
+          { label: '编程', to: '/docs/programming', position: 'left' },
+          { label: '杂项', to: '/docs/others', position: 'left' },
+          { label: 'GitHub', href: 'https://github.com/whoisnian/snippets', position: 'right' },
         ],
       },
       footer: {
         style: 'dark',
         links: [
           {
-            title: 'Docs',
+            title: '分类',
             items: [
-              {
-                label: 'Tutorial',
-                to: '/docs/intro',
-              },
+              { label: '软件', to: '/docs/software' },
+              { label: '系统', to: '/docs/system' },
+              { label: '编程', to: '/docs/programming' },
+              { label: '杂项', to: '/docs/others' },
             ],
           },
           {
-            title: 'Community',
+            title: '链接',
             items: [
-              {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
-                label: 'X',
-                href: 'https://x.com/docusaurus',
-              },
+              { label: 'Source', href: 'https://github.com/whoisnian/snippets' },
+              { label: "GitHub", href: 'https://github.com/whoisnian' },
+              { label: "Blog", href: 'https://whoisnian.com' },
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} whoisnian. Built with Docusaurus.`,
       },
       prism: {
         theme: prismThemes.github,
